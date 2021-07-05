564ed41a77339021a81662e3c58497056a8b8045
Xiaoyun Yang <yangx232@gmail.com>
2021-07-05 18:03:48 -0400

    added fixer for prefer-unicode-ellipsis

--- src/rules/prefer-unicode-ellipsis.js
+++ src/rules/prefer-unicode-ellipsis.js
@@ -6,7 +6,8 @@ module.exports = {
     meta: {
         docs: {
             url: "https://github.com/xiaoyunyang/eslint-plugin-react-i18n/tree/main/docs/rules/prefer-unicode-ellipsis.md"
-        }
+        },
+        fixable: "ellipsis"
     },
     create: function (context) {
         const childWithThreePeriods = (node) => node.children.find(
@@ -15,12 +16,18 @@ module.exports = {
 
         return {
             JSXElement(node) {
-                if(hasOpeningElementTrans(node) && childWithThreePeriods(node)) {
-                    context.report({
-                        node: childWithThreePeriods(node),
-                        message: "Use unicode ellipsis instead of three periods"
-                    });
-                }
+              if (hasOpeningElementTrans(node)) {
+                const offendingNode = childWithThreePeriods(node)
+                if (!offendingNode) return;
+                context.report({
+                  node: offendingNode,
+                  message: "Use unicode ellipsis instead of three periods",
+                  fix: function(fixer) {
+                    const fixedText = offendingNode.raw.replace(/\.{3}/, "…")
+                    return fixer.replaceText(offendingNode, fixedText)
+                  }
+                });
+              }
             }
         }
     }
