3857de61996e2e69a9fcd806f1818c7910f95ce9
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-11-15 22:31:04 -0500

    chore: export all rules with requireindex

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
+const requireIndex = require('requireindex');
+
 module.exports = {
-    rules: {
-        "no-eval-in-placeholder": require("./rules/no-eval-in-placeholder"),
-        "prefer-unicode-ellipsis": require("./rules/prefer-unicode-ellipsis"),
-        "no-useless-string-wrapping": require("./rules/no-useless-string-wrapping"),
-    },
+    rules: requireIndex(`${__dirname}/rules`),
 }
